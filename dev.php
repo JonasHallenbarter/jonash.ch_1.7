6bc0124d46dd7b6c5af243cf5e1a7bcac53df8f7
Jonas Hallenbarter <84071898+JonasHallenbarter@users.noreply.github.com>
2022-04-16 11:17:45 +0200

    .

---
+++ dev.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+	<meta name="robots" content="noindex">
+    <title>DEV-Umgebung</title>
+  </head>
+  <body>
+    <h1>Gitarrenlegende Eric Clapton</h1>
+    <?php
+    // Einige Eckdaten von Mr. Clapton:
+    echo "<p>Geboren am 30.03.1945 in Surrey, England.</p>\n"; // Geburtsdaten
+    echo "<p>Spitzname: Slowhand / englischer Blues- und Rock-Gitarrist.</p>\n"; // Nickname etc.
+    echo "<p>Gilt als einer der \"besten und bedeutendsten\" Gitarristen.</p>\n"; // Einschätzung durch Kollegen
+    echo "<p>Eine Liste der faszinierendsten Lieder wird zu lang...</p>\n";
+    /*
+      Und hier kommt noch der letzte Satz mit Fettdruck
+     */
+    echo "<p><strong>Filmauftritte: u.a. \"Blues Brothers 2000\",  Rockoper \"Tommy\"</strong>.</p>\n";
+    ?>
+	
+
+  </body>
+</html>
---
+++ rechnen.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Übung (Kapitel 3)</title>
+  </head>
+  <body>
+    <h1>Gitarrenlegende Eric Clapton</h1>
+    <?php
+      $liter1 = 16;
+      $liter2 = 23;
+      $liter3 = 34;
+      $preis = 1.35;
+
+      $gesamtzahlung = ($liter1 + $liter2 + $liter3) * $preis;
+      echo $gesamtzahlung
+    ?>
+  </body>
+</html>
